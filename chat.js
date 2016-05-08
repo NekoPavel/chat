3d404a1feaec275a7546ad6f72e3ac3132239eb3
BuildTools <unconfigured@null.spigotmc.org>
2016-05-08 12:15:09 +0200

    initial commit

---
+++ chat.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Pavel on 2016-05-01.
+ */
+
+clients = [];
+
+
+exports.publish = function (message) {
+    console.log("publish %s", message);
+    clients.forEach(function (res) {
+        res.end(message);
+    })
+};
+
+exports.subscribe = function (res) {
+    clients.push(res);
+    console.log("subscribe " + res);
+    console.log(clients.length)
+};
